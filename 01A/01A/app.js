fe098f6d1c84da29f036b7d1e962649e16c090c9
pythonlearner12 <144899168+pythonlearner12@users.noreply.github.com>
2023-11-21 15:00:30 +0100

    Update app.js

--- 01A/01A/app.js
+++ 01A/01A/app.js
@@ -2,12 +2,12 @@
 class App {
     runApplication() {
         
-        let appNaam = ""
+        let appNaam = "appNaam"
         let versienummer = 0.9
-        let versiedatum = ""
-        let autheur = ""
-        let copyright = ""
-        let distributeur = ""
+        let versiedatum = "21/11/2023"
+        let autheur = "Fred"
+        let copyright = "yes"
+        let distributeur = "Fred"
         let darkmode = true
 
         console.log(appNaam, versienummer, versiedatum, autheur, copyright, distributeur, darkmode);
