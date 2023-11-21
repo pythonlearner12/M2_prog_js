e6dac20679073d407d64c6c095aa837db4a266f5
pythonlearner12 <144899168+pythonlearner12@users.noreply.github.com>
2023-11-21 14:59:30 +0100

    Add files via upload
    tweede les

---
+++ 01A/01A/app.js
@@ -0,0 +1,21 @@
+
+class App {
+    runApplication() {
+        
+        let appNaam = ""
+        let versienummer = 0.9
+        let versiedatum = ""
+        let autheur = ""
+        let copyright = ""
+        let distributeur = ""
+        let darkmode = true
+
+        console.log(appNaam, versienummer, versiedatum, autheur, copyright, distributeur, darkmode);
+        //code gaat hier!
+        //print("hello")
+        {
+        }
+    }
+}
+let app = new App();
+app.runApplication();
