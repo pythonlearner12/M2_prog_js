d3a36ba0a359204b946b8dc6ab2a0ef45ac54c44
Fred <36563@ma-web.nl>
2023-11-21 14:45:24 +0100

    idk

---
+++ 01A/app.js
@@ -0,0 +1,12 @@
+
+class App {
+    runApplication() {
+        console.log("hello world!");
+        //code gaat hier!
+        //print("hello")
+        {
+        }
+    }
+}
+let app = new App();
+app.runApplication();
