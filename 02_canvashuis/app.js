e6dc0f47b2ecc0dadec333006c4b256da101d78b
Fred <36563@ma-web.nl>
2023-11-23 14:44:51 +0100

    opdracht 2 gemaakt

--- 02_canvashuis/app.js
+++ 02_canvashuis/app.js
@@ -2,6 +2,10 @@
 
 let canvas = document.getElementById("canvasId");
 let g = canvas.getContext("2d");
-g.fillRect(0,0,100,100);
-console.log(g)
 
+
+g.fillStyle = "red";
+g.fillRect(0,0,canvas.width,canvas.height);
+g.fillStyle = "black";
+g.fillRect(0,0,100,100);
+//console.log(g)
