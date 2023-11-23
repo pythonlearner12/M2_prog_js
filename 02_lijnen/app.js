0a7ab988920b29b47b43bb03da8a8599aa1fffd8
Fred <36563@ma-web.nl>
2023-11-23 15:08:38 +0100

    opdracht 3 gemaakt

---
+++ 02_lijnen/app.js
@@ -0,0 +1,20 @@
+
+
+let canvas = document.getElementById("canvasId");
+let g = canvas.getContext("2d");
+
+
+//g. beginPath()
+//g. moveTo(20,20);
+//g.lineTo(300,300);
+//g. closePath();
+//g. stroke();
+
+g. beginPath()
+g. fillStyle = "yellow";
+g. moveTo(20,20);
+g. lineTo(300,300);
+g.lineTo(580,20);
+g. closePath();
+g. stroke();
+g. fill()
