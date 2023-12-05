399c50f76151d09c49dd11709ec90c1488327546
Fred <36563@ma-web.nl>
2023-12-05 14:20:10 +0100

    01 opdracht gedaan

---
+++ 04_functions/app.js
@@ -0,0 +1,26 @@
+function superMooieGlobalFunction()
+{
+console.log("ik ben global");
+console. log("dus je mag mij overal aanroepen");
+}
+
+superMooieGlobalFunction();
+superMooieGlobalFunction();
+superMooieGlobalFunction();
+
+
+class App {
+    runApplication() {
+
+        //code gaat hier!
+        console.log("hello world")
+        superMooieGlobalFunction();
+        
+        
+    }
+}
+
+let app = new App();
+app. runApplication();
+
+
