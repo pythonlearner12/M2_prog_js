2a9ddaf9a7b5d36cc40f59cddf40b79a3ab23fbf
Fred <36563@ma-web.nl>
2023-12-14 14:47:16 +0100

    opdracht 05 3 gemaakt

--- 05_arrays/app.js
+++ 05_arrays/app.js
@@ -1,27 +1,26 @@
 
 class App {
-    runApplication() {
+    runApplication() 
+    {
         
         let array = ["artiest1", "artiest2", "artiest3"]
+
         array.push("bob dylan")
         array.push("prince")
+
         array.splice(array.indexOf("bob dylan"),1)
         array.splice(array.indexOf("prince"),1)
         
         array.push("artiest4")
         array.push("artiest5")
 
-
         console.log(array)
-
 
         for (let i = 0; i < array. length; i++)
         {
             const element = array[i];
             console.log(`${i} : ${element}`)
         }
-
-        
     }
 
     runApplication2() {
@@ -37,7 +36,39 @@ class App {
         }
         
     }
+
+    runApplication3()
+    {
+        let headers = document.getElementsByTagName("h1");
+        for(let i = 0; i < headers.length; i++)
+        {
+            console.log(headers[i])
+        }
+        let codeObjs = document.getElementsByClassName("bandName");
+        for(let i = 0; i < codeObjs.length; i++)
+        {
+            console.log(codeObjs[i])
+        }
+    }
+
+    runApplication4()
+    {
+        let mijnH1 = document.getElementsByTagName("h1");
+        for(let i = 0; i < mijnH1.length; i++)
+        {
+            console.log(mijnH1[i].innerText)
+        }
+    }
+
+    runApplication5()
+    {
+        let mijnH1 = document.getElementsByTagName("h1");
+        mijnH1[0].innerText = "nieuws ";
+        mijnH1[1].innerText = "reviews";
+        mijnH1[2].innerText = "commentaar ";
+        console.log(mijnH1)
+    }
 }
 
 let app = new App();
-app.runApplication();
+app.runApplication5();
