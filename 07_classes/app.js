c3a00a99d24a33cfecf0f6556c813960e1ca3aa3
Fred <36563@ma-web.nl>
2024-01-11 14:32:02 +0100

    opdracht 1 gemaakt

---
+++ 07_classes/app.js
@@ -0,0 +1,41 @@
+
+class App {
+    runApplication() {
+        
+        console.log("hello world!");
+    }
+}
+
+let app = new App ();
+app.runApplication();
+
+
+
+
+
+class Greet
+{
+    constructor()
+    {
+        console.log("Greetings!");
+    }
+}
+
+let greet = new Greet()
+new Greet();
+new Greet();
+new Greet();
+
+
+class Goodbye
+{
+    constructor()
+    {
+        console.log("goodbye!");
+    }
+}
+
+let goodbye = new Goodbye()
+new Goodbye();
+new Goodbye();
+new Goodbye();
