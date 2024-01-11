99eff1cf7cc7d6b61796adabbcff7da2f09b0c87
Fred <36563@ma-web.nl>
2024-01-11 14:53:39 +0100

    opdracht 3 gemaakt

--- 07_classes/app.js
+++ 07_classes/app.js
@@ -12,7 +12,7 @@
 
 
 
-
+//opdracht 1 en 2
 class Greet
 {
     constructor()
@@ -45,11 +45,45 @@ let goodbye = new Goodbye()
 
 
 
-greet.showgreeting()
-goodbye.showBye()
+//greet.showgreeting()
+//goodbye.showBye()
+//
+//greet.showgreeting()
+//goodbye.showBye()
+//
+//greet.showgreeting()
+//goodbye.showBye()
 
-greet.showgreeting()
-goodbye.showBye()
 
-greet.showgreeting()
-goodbye.showBye()
+
+//opdracht 3
+
+class Greet2 {
+    constructor() {
+        this.greeting = "Greetings!";
+    }
+
+    runApplication() {
+        console.log(this.greeting);
+    }
+}
+
+let greet2 = new Greet2();
+console.log("greeting van buiten: " + greet2.greeting);
+greet2.runApplication();
+
+
+
+class Goodbye2 {
+    constructor() {
+        this.farewell = "farewell!";
+    }
+
+    runApplication() {
+        console.log(this.farewell);
+    }
+}
+
+let goodbye2 = new Goodbye2();
+console.log("greeting van buiten: " + goodbye2.greeting);
+goodbye2.runApplication();
