827562cd13e8cdc413a49a67b3462e39252d08f6
Fred <36563@ma-web.nl>
2024-01-16 14:18:41 +0100

    klaar

--- 07_dinos/app.js
+++ 07_dinos/app.js
@@ -11,22 +11,49 @@ class Dino {
 }
 
 
-
+class User
+{
+    constructor(wiebenik)
+    {
+        this.wiebenik = "wiebenik"
+        console.log(wiebenik)
+    }
+}
 
 
 
 
 class App {
+
+    
     runApplication() {
         {
         
             let dino = new Dino("a", "b", "c");
             console.log(dino.name)
             console.log(dino.bruh)
-            console.log(dino.hi)    
+            console.log(dino.hi)
+
+
+
+
+            let user1 = "";
+            let user2 = "";
+            let user3 = "";
+            let user4 = "";
+            let user5 = "";
+            let user6 = "";
+
+            let patches = new User()
+            patches.wieBenIk();//word niet genoeg uitleg gegeven om te begrijpen wat je moet doen. waardoor mijn code vast fout is.
         }
     }
 }
 
 let app = new App ();
 app.runApplication();
+
+
+
+
+
